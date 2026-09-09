8323f49371cdca056f59b9257b91ee459fe8575e
Enrik24 <enriquemamani12345@gmail.com>
2026-09-09 00:17:25 -0400

    primer deploy v2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/v1',
+  apiUrl: 'https://fashionstore-backend-3wsr.onrender.com/api/v1',
   appName: 'FashionStore',
   version: '1.0.0',
   currency: 'USD',
