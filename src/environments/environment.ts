16b928128b28407bc7472a228957c9a8f2335c20
Enrik24 <enriquemamani12345@gmail.com>
2026-09-14 21:58:45 -0400

    deploy de iteración 2 v2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/v1',
-  wsUrl: 'ws://localhost:8000/api/v1',
+  apiUrl: 'https://fashionstore-backend-3wsr.onrender.com/api/v1',
+  wsUrl: 'wss://fashionstore-backend-3wsr.onrender.com/api/v1',
   appName: 'FashionStore',
   version: '1.0.0',
   currency: 'USD',
